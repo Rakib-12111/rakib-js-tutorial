d772fcc626b0c514769cb03d4aa1d4bec49f5eb1
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-09-04 08:55:09 +0600

    first commit

---
+++ comper.js
@@ -0,0 +1,52 @@
+// var  petaraPrice = 30;
+// var applePrice = 100;
+
+// console.log(petaraPrice > applePrice)
+
+// var rana = 500;
+// var rakib = 1000;
+// console.log(rana < rakib)
+
+// var rahul = 5000;
+// var raton = 200000;
+// console.log( rahul > raton)
+
+// var rana = 100;
+// var rani = 50;
+//  console.log(rana > rani)
+
+// var biryaniPrice = 500;
+// if(biryaniPrice < 300){
+//     console.log('mama give me some birani please')
+// }
+
+// else{
+//     console.log('mama tomar taka kom ace tai hobena')
+// }
+
+// var apple = 500;
+// if(apple < 600){
+//     console.log('mama give me some apple please')
+// }
+
+// else{
+//     console.log('mama tmar taka kom ace hove na basay jay')
+// }
+
+// var weight = 50;
+// if(weight < 40){
+//     console.log('ame nife niya asbo mathay kota')
+// }
+
+// else{
+//     console.log('ami gari bara koita niya asbo karon amr aozon besi ase')
+// }
+
+// var weight = 100;
+// if(weight < 200){
+//     console.log('ami nizay niya asbo ')
+// }
+
+// else{
+//     console.log('ami gari vara kota niya asbo karon aozon besi ase')
+// }
