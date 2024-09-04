ac14164d2d603ec59e0ff7e6337712b6838a99cf
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-09-04 09:32:57 +0600

    secend commit

--- comper.js
+++ comper.js
@@ -50,3 +50,40 @@
 // else{
 //     console.log('ami gari vara kota niya asbo karon aozon besi ase')
 // }
+
+// const salary = 25000;
+// const isBCS = true;
+// if(salary < 20000){
+//     console.log('ami tomake amr me dita bita dibo')
+// }
+
+// else{
+//     console.log('ami tomake amr me dibona')
+// }
+
+
+
+// const salary = 25000;
+// const isBSC = true;
+// hight = 100;
+// if(salary > 20000 && isBSC ===true && hight > 90){
+//     console.log('tmi supatto ami tomake amr me dito')
+// }
+
+// else{
+//     console.log('tmi kharap ami tomake amr me dibo na tim basy jay')
+// }
+
+
+
+
+
+// const price = 5000;
+// if(price >=5000){
+//     // 10 % discount
+//     const discount = price * 10 /100;
+//     const payAmout = price - discount;
+//     console.log(payAmout)
+//     console.log(discount)
+//     console.log('I will go to market')
+// }
