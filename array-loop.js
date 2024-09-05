0bcfc222db38afdc2052e974e9f6f984854fc5e8
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-09-05 16:19:42 +0600

    new file added

---
+++ array-loop.js
@@ -0,0 +1,71 @@
+// const numbers = [1, 2, 3, 4, 5, 6, 7,]
+// console.log(numbers)
+// numbers.reverse ();
+// console.log(numbers)
+
+// !Task 1 complite
+// const colors = [ 'red', 'blue', 'green', 'yellow', 'orange']
+// console.log(colors)
+// colors.reverse();
+// console.log(colors)
+
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+// for(const num of colors){
+//     console.log(num);
+    
+// }
+
+// const colors = ['red', 'blue', 'yellow', 'green', 'orange']
+// for(const num of colors){
+//     console.log(num);
+    
+// }
+
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+// console.log(colors)
+// colors.reverse();
+// console.log(colors)
+
+
+
+// const numbers = [ 12, 98, 5, 41, 23, 78, 46,]
+// const evenNumbers = []
+
+// for(let i =0 ; i < numbers.length; i ++){
+//     if( numbers [i] % 2=== 0){
+//         evenNumbers.push(numbers[i]);
+//     }
+// }
+
+// console.log(evenNumbers)
+
+// const numbers = [ 12, 98, 5, 41, 23, 78, 46]
+// const evenNumbers = [];
+// for(let i = 0; i < numbers.length; i ++){
+//     if(numbers [i] % 2===0){
+//         evenNumbers.push(numbers [i])
+//     }
+// }
+
+// console.log(evenNumbers)
+
+
+
+
+
+
+
+// var numbers = [ 'Tom', 'Tim', 'Tin', 'Tik']
+// var rakib = [];
+// for( var nom of numbers){
+//     rakib += nom;
+// }
+
+// console.log(rakib)
+
+
+const statement = 'I am a gard working person';
+console.log(statement)
+const reversed = statement.split (' ').reverse().join(' ');
+console.log(reversed)
+
--- modul-20.js
+++ modul-20.js
@@ -35,11 +35,76 @@
 
 
 
-const numbers = [ 1, 2, 3, 4, 5, 6, 7,]
-console.log(numbers)
-for(const num of numbers){
-    console.log(num)
-}
+// const numbers = [ 1, 2, 3, 4, 5, 6, 7,]
+// console.log(numbers)
+// for(const num of numbers){
+//     console.log(num)
+// }
+
+// const numbers = [ 1, 2, 3, 4, 5, 6, 7, ]
+// console.log(numbers)
+// numbers.reverse ()
+// console.log(numbers)
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7]
+// numbers.reverse()
+// console.log(numbers);
+
+// const numbers = [ 1, 2, 3, 4, 5, 6,7]
+// numbers.reverse()
+// console.log(numbers);
+
+
+
+
+// const count = [ 1, 2,3, 4, 5, 6, 7,]
+// const rev_count = [];
+// for(const num of count){
+//     console.log(num)
+//     rev_count.push (num)
+// }
+// console.log(rev_count)
+
+
+
+// const numbers = [ 1, 2, 3, 4, 5, 6, 7,]
+// const rev_numbers = []
+// for(const num of numbers){
+//     console.log(num)
+// }
+
+// console.log(rev_numbers)
+
+
+
+
+// const numbers = [ 1, 2, 3, 4, 5, 6, 7,]
+// const rev_numbers = [];
+// for(const num of numbers){
+//     console.log(num);
+//     rev_numbers.unshift(num)
+    
+// }
+// console.log(rev_numbers)
+
+// const numbers = [ 1, 2, 3, 4, 5, 6,7,]
+// const rev_numbers = [];
+// for(const num of numbers){
+//     console.log(num);
+//     rev_numbers.unshift(num)
+    
+// }
+
+
+
+// const numbers = [ 1, 2, 3, 4, 5, 6, 7]
+// const  reversed_numbers = [];
+// for(let i = 0; i < numbers.length; i++ ){
+//     const num = numbers [i]
+//     reversed_numbers.unshift(i)
+//     console.log(reversed_numbers);
+    
+// }
 
 
 
@@ -49,7 +114,30 @@ for(const num of numbers){
 
 
 
+// const numbers = [ 1, 12, 3, 4, 5,6, 7]
+// for(let i = 0; i < numbers.length; i ++){
+//     console.log(i);
+    
+// }
 
+
+// const numbers = [ 1, 2, 3,4, 5, 6, 7]
+// for(let i= numbers.length -1; i >= 0; i --){
+//     console.log(i);
+    
+// }
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7]
+// for(let i = numbers.length -1; i >= 1 i --){
+//     console.log(i);
+    
+// }
+
+
+
+
+let rakib = ' hello world'
+console.log(rakib.toUpperCase())
 
 
 
