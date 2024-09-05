114212de803c43771f6771dc149cafd36a0c7f5f
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-09-05 08:45:02 +0600

    reverse numbers added file

--- modul-20.js
+++ modul-20.js
@@ -17,8 +17,40 @@
 //     console.log(frineds [i])
 // }
 
-const frineds = [ 'Rakib', ' Rahul', ' Tufan', 'Rana', 'Rakibul']
-for(let i = 0; i < frineds.length; i ++){
-    console.log(i)
-    console.log(frineds [i])
+// const frineds = [ 'Rakib', ' Rahul', ' Tufan', 'Rana', 'Rakibul']
+// for(let i = 0; i < frineds.length; i ++){
+//     console.log(i)
+//     console.log(frineds [i])
+// }
+
+
+
+// const numbers = [ 11, 12, 13, 14, 15, 16, 17, 18, 19,]
+// for(let i = 0; i < numbers.length; i ++){
+//     console.log(i)
+//     console.log(numbers [i])
+// }
+
+
+
+
+
+const numbers = [ 1, 2, 3, 4, 5, 6, 7,]
+console.log(numbers)
+for(const num of numbers){
+    console.log(num)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
