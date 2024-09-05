45aa919d7f1a003f166de6b87cb527294f4ce988
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-09-05 08:37:39 +0600

    first commit

--- modul-20.js
+++ modul-20.js
@@ -1,4 +1,24 @@
-const frineds = [ 'Elon', 'Bill', 'Mark', 'Waren']
-for(const friend of frineds){
-    console.log(friend)
+// const frineds = [ 'Elon', 'Bill', 'Mark', 'Waren']
+// for(const friend of frineds){
+//     console.log(friend)
+// }
+
+
+// for(let i =0 ; i < 10; i ++){
+//     console.log(i)
+//     console.log(frineds [i])
+// }
+
+
+
+// const frineds = [ 'Rana', 'Rakib', 'Tufan', 'Lalon']
+// for( let i = 0 ; i <frineds.length; i ++){
+//     console.log(i);
+//     console.log(frineds [i])
+// }
+
+const frineds = [ 'Rakib', ' Rahul', ' Tufan', 'Rana', 'Rakibul']
+for(let i = 0; i < frineds.length; i ++){
+    console.log(i)
+    console.log(frineds [i])
 }
